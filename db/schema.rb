c8cf2ee1657babc963ee234dfd73291ed18ff890
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-08-04 13:20:05 -0300

    index and db update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_04_150328) do
+ActiveRecord::Schema.define(version: 2022_08_04_154122) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2022_08_04_150328) do
     t.integer "calories"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "directions"
+    t.decimal "cost"
   end
 
 end
