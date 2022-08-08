9d517979b61502d2ea6c0e8fdfb7ee615acba737
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-08-08 12:17:40 -0300

    light helper

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,10 @@
 module RecipesHelper
+
+  def formata_caloria(recipe)
+    if recipe.light?
+      content_tag(:strong, "Light (less than 100 calories)")
+    else
+      recipe.calories
+    end
+  end
 end
